61800d9f47503bc5f1a6e7c4b6cc1d729f1a1bbc
Łukasz Włodarczyk <cyberwlodarczyk@proton.me>
2026-07-20 11:08:19 +0200

    fix .gitignore ignoring tests/*.h

---
+++ tests/exchange.h
@@ -0,0 +1,30 @@
+#ifndef EXCHANGE_H
+#define EXCHANGE_H
+
+#include <stdint.h>
+
+int exchange_kyber();
+
+int exchange_rkem();
+
+int exchange_xrkem();
+
+int exchange_yrkem();
+
+int exchange_nice_pake_correct();
+
+int exchange_nice_pake_incorrect();
+
+int exchange_nice_pake_re_correct();
+
+int exchange_nice_pake_re_incorrect();
+
+int exchange_tempo_correct();
+
+int exchange_tempo_incorrect();
+
+int exchange_tempo_re_correct();
+
+int exchange_tempo_re_incorrect();
+
+#endif
---
+++ tests/test.h
@@ -0,0 +1,8 @@
+#ifndef TEST_H
+#define TEST_H
+
+int test_run(const char *name, int t());
+
+void test_speed(const char *name, int t());
+
+#endif
